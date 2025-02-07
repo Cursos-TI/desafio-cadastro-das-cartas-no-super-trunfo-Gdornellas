b90b1143d7d7384287729c778ae26fed6090e181
Gdornellas <glaucodornellas@gmail.com>
2025-02-07 18:38:16 +0000

    Montagem do código

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -19,4 +19,49 @@ int main() {
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
     return 0;
+
+    int populacao;
+    float area;
+    float pib;
+    int pontosturisticos;
+    char cidade [50];
+    char codigo [3];
+    char pais [20];
+    char estado [20];
+
+    printf ("País: \n");
+    scanf ("%s", &pais);
+
+    printf ("Cadastre o estado: /\n");
+    scanf ("%s", &estado);
+
+    printf ("Cadastre a cidade: /\n");
+    scanf ("%s", &cidade);
+
+    printf ("Cadastre o código da carta: /\n");
+    scanf ("%s", &codigo);
+
+    printf ("Cadastre a população da cidade: /\n");
+    scanf ("%d", &populacao);
+
+    printf ("Cadastre a área da cidade: /\n");
+    scanf ("%f", &area);
+
+    printf ("Cadastre o o PIB da cidade: /\n");
+    scanf ("%f", &pib);
+
+    printf ("Cadastre a quantidade de pontos turísticos da cidade: /\n");
+    scanf ("%d", &pontosturisticos);
+
+    printf ("Código da carta: %s\n", codigo);
+    printf ("País: %s\n", pais);
+    printf ("Estado: %s\n", estado);
+    printf ("Cidade: %s\n", cidade);
+    printf ("População: %d\n", populacao);
+    printf ("Área da cidade: %d\n", area);
+    printf ("PIB da cidade: %s\f", pib);
+    printf ("Pontos turísticos: %d\n", pontosturisticos);
+
+
+
 }
